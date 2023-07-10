1bde5477b1bfb83181b22a0528db04ebc599ae68
Uchenna Okehie <dizzybones007@yahoo.com>
2023-07-10 14:38:48 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from   flask_migrate import Migrate
+from apps.config import config_dict
+from apps import create_app, db
 
-app = Flask(__name__)
+app = create_app(app_config)
+Migrate(app, db)
 
 if __name__ == "__main__":
     app.run()
